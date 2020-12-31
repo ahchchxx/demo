7ce7579dc46287bbaab1d9a794616c6e30e26be7
ahchchxx <ahchchxx@126.com>
2020-12-31 15:34:01 +0800

    mq

---
+++ MQ/src/main/java/com/example/MQDev/Broker.java
@@ -0,0 +1,13 @@
+package com.example.MQDev;
+
+import org.apache.rocketmq.broker.BrokerStartup;
+
+public class Broker {
+
+    // Environment Variables:
+    //      ROCKETMQ_HOME=D:\workstation\demo\MQ
+    public static void main(String[] args) {
+        BrokerStartup.main(args);
+    }
+
+}
---
+++ MQ/src/main/java/com/example/MQDev/NameServer.java
@@ -0,0 +1,13 @@
+package com.example.MQDev;
+
+import org.apache.rocketmq.namesrv.NamesrvStartup;
+
+public class NameServer {
+
+    // Environment Variables:
+    //      ROCKETMQ_HOME=D:\workstation\demo\MQ
+    public static void main(String[] args) {
+        NamesrvStartup.main(args);
+    }
+
+}
