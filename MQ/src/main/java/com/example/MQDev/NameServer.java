c2f4c01d51c36f5deb3b2ecc202b5ba00b12a772
ahchchxx <ahchchxx@126.com>
2021-01-02 18:32:36 +0800

    Revert "Revert "Update NameServer.java""
    This reverts commit 6ec266741d6608580256af904ae97a091d4756cd.

--- MQ/src/main/java/com/example/MQDev/NameServer.java
+++ MQ/src/main/java/com/example/MQDev/NameServer.java
@@ -6,6 +6,9 @@ public class NameServer {
 
     // Environment Variables:
     //      ROCKETMQ_HOME=D:\workstation\demo\MQ
+    // org.apache.rocketmq.common.namesrv.NamesrvConfig 里获取:
+    //  private String rocketmqHome = System.getProperty("rocketmq.home.dir", System.getenv("ROCKETMQ_HOME"));
+    //     System.getenv("ROCKETMQ_HOME") instanceof String  判断类型
     public static void main(String[] args) {
         NamesrvStartup.main(args);
     }
