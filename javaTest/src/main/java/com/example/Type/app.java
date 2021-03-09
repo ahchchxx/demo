be383658b7b1df7fab1cca46a71fe5bfb97e36ab
ahchchxx <ahchchxx@126.com>
2021-03-09 17:10:35 +0800

    java类型的长度范围example

---
+++ javaTest/src/main/java/com/example/Type/app.java
@@ -0,0 +1,23 @@
+package com.example.Type;
+
+/**
+ * 数据类型，及其所占体积大小
+ */
+public class app {
+    public static void main(String[] args) {
+        print("Short.MAX_VALUE", Short.MAX_VALUE); // 32767       2^15 -1
+        print("Short.MIN_VALUE", Short.MIN_VALUE); // -32768      - 2^15
+
+        print("Integer.MAX_VALUE", Integer.MAX_VALUE); // 2147483647    2^31 -1
+        print("Integer.MIN_VALUE", Integer.MIN_VALUE); // -2147483648   - 2^31
+
+        print("Byte: ", Byte.SIZE); // 8 bit     1byte
+        print("Short: ", Short.SIZE); // 16
+        print("Character: ", Character.SIZE); // 16
+        print("Integer: ", Integer.SIZE); // 32
+    }
+
+    public static void print(String str, Object obj) {
+        System.out.println(str + " : " + obj);
+    }
+}
