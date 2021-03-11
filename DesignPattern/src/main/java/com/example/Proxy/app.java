971332ca3eba817f1d13e7254b4012c08c4be8a6
ahchchxx <ahchchxx@126.com>
2021-03-11 14:44:53 +0800

    Update app.java

--- DesignPattern/src/main/java/com/example/Proxy/app.java
+++ DesignPattern/src/main/java/com/example/Proxy/app.java
@@ -2,11 +2,11 @@ package com.example.Proxy;
 
 /**
  * Proxy example
+ * 静态代理，仅代理了一个类
  */
 public class app {
     public static void main(String[] args) {
         Image img = new ProxyImage("abc.jpg");
-        img.display();
         img.display();
     }
 }
